b07c473f5f75ca81c0ab6afade1d8281f75202fb
ibanb04 <ibanb040@gmail.com>
2017-09-26 14:45:52 -0500

    new

---
+++ Proyecto Realidad Aumentada/Assets/scripts/movimiento.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimiento : MonoBehaviour {
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0f, 1f, 0f));
+
+    }
+}
