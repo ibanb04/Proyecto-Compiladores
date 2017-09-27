96643b0ac99091cae49880daa0751a376ecd6ced
ibanb04 <ibanb040@gmail.com>
2017-09-27 13:16:19 -0500

    nw

---
+++ Proyecto Realidad Aumentada/Assets/scripts/rotar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotar : MonoBehaviour {
+    public Transform target;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 relativePos=(target.position+new Vector3(0,1.5f,0));
+        Quaternion rotation = Quaternion.LookRotation(relativePos);
+        Quaternion current=transform.localRotation;
+
+        transform.localRotation = Quaternion.Slerp(current,rotation,Time.deltaTime);
+    }
+}
